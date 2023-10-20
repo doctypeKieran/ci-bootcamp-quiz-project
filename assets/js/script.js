f04d328aa415a09517a94e18ca31e4bd831573d3
Kieran Hillman <developedbykieran@gmail.com>
2023-10-20 09:27:12 +0000

    Add variables for DOM elements and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("SCRIPT DEPLOYED")
+// DOM Elements
+const quizContainer = document.getElementById('quiz-container');
+const nextButton = document.getElementById('next-button');
+
+// State variables
+let currentQuestionIndex = 0;
+
+let questions = [
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Jupiter", "Mars", "Saturn"],
+        correctAnswer: "Mars"
+    },
+    {
+        question: "How many contintents are there?",
+        options: ["5", "6", "7", "8", "9"],
+        correctAnswer: "7"
+    }
+]
