c1a013d2de43bfb8347a519ee73efd617178db63
Kieran Hillman <developedbykieran@gmail.com>
2023-10-22 14:39:05 +0000

    Add more questions, add correct/incorrect answer counters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // DOM Elements
 const quizContainer = document.getElementById('quiz-container');
 const nextButton = document.getElementById('next-button');
+const correctAnswers = document.getElementById('correct-answers');
+const incorrectAnswers = document.getElementById('incorrect-answers');
+const restartQuizBtn = document.getElementById('restart-quiz-btn');
+
+// Variables for correct and incorrect answers
+let numberOfCorrectAnswers = 0;
+let numberOfIncorrectAnswers = 0;
 
 // State variables
 let currentQuestionIndex = 0;
@@ -15,6 +22,26 @@ let questions = [
         question: "How many contintents are there?",
         options: ["5", "6", "7", "8", "9"],
         correctAnswer: "7"
+    },
+    {
+        question: "What animal is known as 'the king of the jungle'?",
+        options: ["Lion", "Tiger", "Jaguar", "Elephant", "Monkey"],
+        correctAnswer: "Lion"
+    },
+    {
+        question: "What year was Roald Dahl born?",
+        options: ["2001", "1916", "1991", "1960"],
+        correctAnswer: "1916"
+    },
+    {
+        question: "How many planets are in the solar system?",
+        options: ["7", "9", "4", "8"],
+        correctAnswer: "8"
+    },
+    {
+        question: "What house is Harry Potter in?",
+        options: ["Hufflepuff", "Slytherin", "Gryffindor", "Ravenclaw"],
+        correctAnswer: "Gryffindor"
     }
 ]
 
@@ -22,11 +49,23 @@ let questions = [
 
 nextButton.addEventListener("click", loadNextQuestion);
 
+restartQuizBtn.addEventListener("click", () => {
+    currentQuestionIndex = 0;
+    initialiseQuiz();
+    numberOfCorrectAnswers = 0;
+    numberOfIncorrectAnswers = 0;
+    correctAnswers.innerText = numberOfCorrectAnswers;
+    incorrectAnswers.innerText = numberOfIncorrectAnswers;
+    restartQuizBtn.style.display = "none";
+})
+
 // Initialise the quiz
 initialiseQuiz();
 
 // Display the first question
 function initialiseQuiz() {
+    correctAnswers.innerText = numberOfCorrectAnswers;
+    incorrectAnswers.innerText = numberOfIncorrectAnswers;
     displayQuestion(questions[currentQuestionIndex]);
 }
 
@@ -61,12 +100,16 @@ function createOptionButton(option) {
     return button;
 }
 
-    function handleOptionClick(optionSelected) {
+function handleOptionClick(optionSelected) {
     const correctAnswer = questions[currentQuestionIndex].correctAnswer;
     if (optionSelected === correctAnswer) {
         alert("CORRECT!");
+        numberOfCorrectAnswers++;
+        correctAnswers.innerText = numberOfCorrectAnswers;
     } else {
         alert("You plonker Rodney!");
+        numberOfIncorrectAnswers++;
+        incorrectAnswers.innerText = numberOfIncorrectAnswers;
     }
     loadNextQuestion();
 }
@@ -83,5 +126,6 @@ function loadNextQuestion() {
 
 function endQuiz() {
     quizContainer.innerHTML = `<h1>Game Over!!!</h1>`;
+    restartQuizBtn.style.display = "inline-block";
     nextButton.classList.add("d-none");
 }
